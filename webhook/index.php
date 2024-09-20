3b90c3171f78b2d78252a63d13ac168151648901
Maicom Ferreira <maicomferre3242@gmail.com>
2024-09-20 11:18:36 -0300

    add features

--- webhook/index.php
+++ webhook/index.php
@@ -1,12 +1,10 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $input = file_get_contents("php://input");
-    $event = json_decode($input, true);
+$input = file_get_contents("php://input");
+$event = json_decode($input, true);
 
-    file_put_contents('webhook.log', print_r($event, true), FILE_APPEND);
+file_put_contents('webhook.log', print_r($event, true), FILE_APPEND);
 
-    http_response_code(200);
-} else {
-    http_response_code(405); // Método não permitido
-}
+http_response_code(200);
+
+?>
